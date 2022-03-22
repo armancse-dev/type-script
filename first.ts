b3c917784def500ea0153c992228d033ac5b2e1e
armancse-dev <armanhossaincse7@gmail.com>
2022-03-22 21:03:14 +0600

    typescript type number string boolean

--- first.ts
+++ first.ts
@@ -1,3 +1,14 @@
 const myNumber = 5;
 const myName = 'Arman';
 console.log("Hello From Typescript", myNumber,myName);
+
+// let money = 500;
+// let honey = '';
+// let kony = false;
+
+let money:number = 5;
+let honey:string = 'String Honey tesing';
+let kony:boolean = true;
+
+const age:number = 21;
+
